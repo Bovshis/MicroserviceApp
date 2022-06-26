105c796fd56848117df4c64d1f1b27efcefdfd77
Даниил Бовшис <daniilbovshis@outlook.com>
2022-06-26 11:05:59 +0300

    Add Sql Server database

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -6,16 +6,32 @@ namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app, bool isProduction)
         {
             using var serviceScope = app.ApplicationServices.CreateScope();
-            SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>()!);
+            SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>()!, isProduction);
         }
 
-        private static void SeedData(AppDbContext db)
+        private static void SeedData(AppDbContext db, bool isProduction)
         {
+            if (isProduction)
+            {
+                Console.WriteLine("--> Attempting to apply migrations...");
+                try
+                {
+                    db.Database.Migrate();
+                    Console.WriteLine("--> Done migrations!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Could not run migration: {e.Message}");
+                }
+            }
+
             if (!db.Platforms.Any())
             {
+                Console.WriteLine("--> Seeding Data...");
+
                 db.Platforms.AddRange(
                     new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
                     new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
@@ -23,6 +39,10 @@ namespace PlatformService.Data
                 );
 
                 db.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
             }
         }
     }
